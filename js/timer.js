e0c4e149348162bcf848b860bdf189b5ae5385d1
Jossl123 <jossl1.gau@gmail.com291204Jg291204JG>
2021-06-29 13:20:43 +0200

    add mobile click detection

--- js/timer.js
+++ js/timer.js
@@ -31,6 +31,31 @@ document.addEventListener('keydown', function(key) {
     }
 })
 
+document.addEventListener('touchstart', function() {
+    if (solving) { //solved !
+        solving = false
+        document.getElementById("timer").innerHTML = timer / 100
+        document.getElementById("timer").style.color = "black"
+        times.push(timer / 100)
+        localStorage.setItem("times", JSON.stringify(times))
+        document.getElementById("unsaveTime").style.display = "flex"
+    } else {
+        document.getElementById("timer").innerHTML = '0.00'
+        document.getElementById("unsaveTime").style.display = "none"
+        document.getElementById("timer").style.color = "green"
+    }
+})
+
+document.addEventListener('touchend', function() {
+    if (haveSolved) {
+        timer = 0
+        solving = true
+        haveSolved = false
+    } else {
+        haveSolved
+    }
+})
+
 setInterval(function() {
     if (solving) {
         timer++
